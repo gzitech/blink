da0551505180f23c31db0968c617f716933bd22b
sskycn <sskycn@gmail.com>
2024-08-04 20:26:13 +0800

    blink pe2,pe3,pe4

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,6 +94,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_SET);
+    HAL_Delay(200);
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_RESET);
+    HAL_Delay(200);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
